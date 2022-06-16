685c45a3eb5fb43ffca521c75084c82b070d5216
Tanya Raj <tanyaraj@Tanyas-MacBook-Air.local>
2022-06-16 16:56:06 +0900

    Setting DTO's and Services

--- videoapp/src/main/java/com/example/videoapp/entity/VideoDTO.java
+++ videoapp/src/main/java/com/example/videoapp/entity/VideoDTO.java
@@ -2,6 +2,8 @@ package com.example.videoapp.entity;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Builder
 @Getter
 @Setter
@@ -12,5 +14,9 @@ public class VideoDTO {
     private Integer fileId ;
 
     private String givenName ;
+
+    private double duration;
+
+    private Date uploadDate;
 
 }
